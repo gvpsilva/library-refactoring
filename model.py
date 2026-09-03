691d7584aa0700a52cdf8e915ea72079f856cc17
gvpsilva <gvpsilvaa@gmail.com>
2026-09-02 23:01:03 -0300

    Commit 3: Extracting method get_charge from Client.statement.

--- model.py
+++ model.py
@@ -8,25 +8,25 @@ class Book:
         self.title = title
         self.price_code = price_code
 
-    
-    def title(self) -> str:
+    def get_title(self) -> str:
         return self.title
 
-  
-    def price_code(self) -> int:
+    def get_price_code(self) -> int:
         return self.price_code
 
+
 class Rental:
+
     def __init__(self, book: Book, days_rented: int):
         self.book = book
         self.days_rented = days_rented
 
-
-    def book(self) -> Book:
+    def get_book(self) -> Book:
         return self.book
 
-    def days_rented(self) -> int:
+    def get_days_rented(self) -> int:
         return self.days_rented
+
 
 class Client:
 
@@ -37,40 +37,38 @@ class Client:
     def add_rental(self, rental: Rental):
         self.rentals.append(rental)
 
-    def name(self) -> str:
+    def get_name(self) -> str:
         return self.name
 
-    def statement(self) -> str:
+    def get_charge(self, rental: Rental):
+        amount = 0
+        if rental.book.price_code == Book.REGULAR:
+            amount += 2
+            if rental.days_rented > 2:
+                amount += (rental.days_rented - 2) * 1.5
+        elif rental.book.price_code == Book.NEW_RELEASE:
+            amount += rental.days_rented * 3
+        elif rental.book.price_code == Book.CHILDREN:
+            amount += 1.5
+            if rental.days_rented > 3:
+                amount += (rental.days_rented - 3) * 1.5
 
+        return amount
+
+    def statement(self) -> str:
         total_amount = 0
         frequent_renter_points = 0
         result = f"Rental summary for {self.name}\n"
-        
-        for rental in self.rentals:
-            amount = 0
-            
-            # determine amounts for each line
-            if rental.book.price_code == Book.REGULAR:
-                amount += 2
-                if rental.days_rented > 2:
-                    amount += (rental.days_rented - 2) * 1.5
-            elif rental.book.price_code == Book.NEW_RELEASE:
-                amount += rental.days_rented * 3
-            elif rental.book.price_code == Book.CHILDREN:
-                amount += 1.5
-                if rental.days_rented > 3:
-                    amount += (rental.days_rented - 3) * 1.5
 
-            # add frequent renter points
+        for rental in self.rentals:
+            amount = self.get_charge(rental)
             frequent_renter_points += 1
             if rental.book.price_code == Book.NEW_RELEASE and rental.days_rented > 1:
                 frequent_renter_points += 1
 
-            # show each rental result
             result += f"- {rental.book.title}: {amount}\n"
             total_amount += amount
-        
-        # show total result
+
         result += f"Total: {total_amount}\n"
         result += f"Points: {frequent_renter_points}"
         return result
